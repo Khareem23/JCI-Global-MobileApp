3c975b219174bff6ab911d77eb2316c31ba6b1e3
esanjubabatunde <tundeesanju@gmail.com>
2022-02-24 07:26:26 +0100

    commit local

--- lib/UI/auth/register/screens/basic_info.dart
+++ lib/UI/auth/register/screens/basic_info.dart
@@ -16,7 +16,7 @@ class BasicInfoScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final _formKey = useState(GlobalKey<FormState>());
-    final initialCountry = useState('NG');
+    final initialCountry = useState('AUS');
     PhoneNumber number = PhoneNumber(isoCode: initialCountry.value);
     var _genders = ['Male', 'Female'];
     var _accountTypes = ['Individual Account', 'Business Account'];
